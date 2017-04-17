cf431fb9115df74af94338a6af39165e632ae580
Saad Ahmad <xcode@fasts-macbook-pro-2.local>
2017-04-17 15:56:15 -0400

    few changes to storyboards

---
+++ Sheridan United/UserViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserViewController.h
+//  Sheridan United
+//
+//  Created by Xcode User on 2017-04-17.
+//  Copyright © 2017 Sheridan College. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserViewController : UIViewController
+
+@end
---
+++ Sheridan United/UserViewController.m
@@ -0,0 +1,37 @@
+//
+//  UserViewController.m
+//  Sheridan United
+//
+//  Created by Xcode User on 2017-04-17.
+//  Copyright © 2017 Sheridan College. All rights reserved.
+//
+
+#import "UserViewController.h"
+
+@interface UserViewController ()
+
+@end
+
+@implementation UserViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
